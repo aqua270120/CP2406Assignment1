9660384cae91033eaed68d50485d59ac368b4883
Nguyen Le Huy Bao <nguyenlehb1808@gmail.com>
2020-11-30 00:52:51 +0700

    Finished with object Car

--- src/Car.java
+++ src/Car.java
@@ -1,22 +1,23 @@
 public class Car {
+    protected  float breadth;
+    private final int NEXT_ROAD_INDEX = 0;
+    private final int START_POSITION = 0;
     //Attributes
     protected float length;
-    protected float breadth;
     protected String id;
     protected int speed;
     protected int position;
     protected Road currentRoad = new Road();
-
     //Get set methods
 
     //Constructors
-    public Car(String id) {
+    public Car(String id, Road currentRoad) {
         this.id = id;
-//        this.currentRoad = currentRoad;
+        this.currentRoad = currentRoad;
         length = 1f;
         breadth = length * 0.5f;
         speed = 1;
-        position = 1;
+        position = 0;
 
     }
 
@@ -80,7 +81,35 @@ public class Car {
     //Input output methods
 
     public void showOutPut() {
-        System.out.println("This is car, length: " + this.length);
+        System.out.println("Car " + this.id + " going: " + this.speed + "dm/s on road "
+                + this.currentRoad.getId() + " at position " + this.position);
     }
+
     //Business methods
+    public void move() {
+        this.speed = this.currentRoad.getSpeedLimit();
+        if (!this.currentRoad.getLigthsOnRoad().isEmpty() && this.position == this.currentRoad.getLigthsOnRoad().get(0).getPosition()[0]) {
+            if (this.currentRoad.getLigthsOnRoad().get(0).getState().equals("RED")) {
+                this.speed = 0;
+            } else {
+                if (!this.currentRoad.getConnectedRoads().isEmpty()) {
+                    this.currentRoad.getCarsOnRoad().remove(this);
+                    this.currentRoad = this.currentRoad.getConnectedRoads().get(NEXT_ROAD_INDEX);
+                    this.currentRoad.getCarsOnRoad().add(this);
+                    this.position = START_POSITION;
+                }
+            }
+        } else {
+            if (this.currentRoad.getLength() > this.getPosition()) {
+                this.position += this.speed;
+            } else if (this.currentRoad.getLength() < this.getPosition()) {
+                this.speed = 0;
+            } else {
+                this.speed = 0;
+            }
+        }
+
+
+    }
 }
+
